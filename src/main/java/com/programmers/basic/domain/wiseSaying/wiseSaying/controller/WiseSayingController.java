8fb23ecf89bde71cfb55a6a191cd66bc8fef1362
davinyakma <poohcdv3652@naver.com>
2024-12-12 12:16:54 +0900

    feat: @controller + @responsebody => @RestController

--- src/main/java/com/programmers/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
+++ src/main/java/com/programmers/basic/domain/wiseSaying/wiseSaying/controller/WiseSayingController.java
@@ -3,14 +3,12 @@ package com.programmers.basic.domain.wiseSaying.wiseSaying.controller;
 import com.programmers.basic.domain.wiseSaying.wiseSaying.entity.WiseSaying;
 import com.programmers.basic.domain.wiseSaying.wiseSaying.service.WiseSayingService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-@ResponseBody
+@RestController
 @RequiredArgsConstructor
 public class WiseSayingController {
     private final WiseSayingService wiseSayingService;
